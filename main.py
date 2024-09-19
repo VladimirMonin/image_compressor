74cef6a0ec7f4cf7efc76ebc32b372044337fd31
v <v@mail.ru>
2024-09-19 12:28:16 +0500

    feat: рабочая версия

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ QUALITY = 80  # Можно настроить качество сжатия
 def compress_image(input_path, output_path):
     with Image.open(input_path) as img:
         img.save(output_path, "HEIF", quality=QUALITY)
+    print(f"Сжато: {input_path} -> {output_path}")
 
 def process_directory(directory):
     for root, dirs, files in os.walk(directory):
@@ -18,13 +19,16 @@ def process_directory(directory):
 
 def main(input_path):
     register_heif_opener()
+    input_path = input_path.strip('"')
     
-    if os.path.isfile(input_path):
-        if input_path.lower().endswith(('.jpg', '.jpeg', '.png')):
+    if os.path.exists(input_path):
+        if os.path.isfile(input_path):
+            print(f"Обрабатываем файл: {input_path}")
             output_path = os.path.splitext(input_path)[0] + '.heic'
             compress_image(input_path, output_path)
-    elif os.path.isdir(input_path):
-        process_directory(input_path)
+        elif os.path.isdir(input_path):
+            print(f"Обрабатываем директорию: {input_path}")
+            process_directory(input_path)
     else:
         print("Указанный путь не существует")
 
